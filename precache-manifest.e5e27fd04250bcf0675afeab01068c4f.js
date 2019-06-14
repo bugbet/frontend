c2388156c8c92443dde9f033807021d80d2baab4
bugbet <contact@bugbet.io>
2019-06-14 21:22:42 +0200

    init

---
+++ precache-manifest.e5e27fd04250bcf0675afeab01068c4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d7e694cff88430f2813",
+    "url": "/static/css/main.597b0e37.chunk.css"
+  },
+  {
+    "revision": "1d7e694cff88430f2813",
+    "url": "/static/js/main.1d7e694c.chunk.js"
+  },
+  {
+    "revision": "5f39e60181be915eba11",
+    "url": "/static/js/1.5f39e601.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0039d59dd4e7b70ac9821cd49ce4f60",
+    "url": "/index.html"
+  }
+];
